3211d5669082927f63e5e9d3a03ae6530b57e47e
Angelo Romano <angelodaroma00@gmail.com>
2024-03-21 01:10:53 +0100

    added build

---
+++ build/Telem.py
@@ -0,0 +1,43 @@
+import serial
+import math
+import serial.tools.list_ports
+
+serialInst = serial.Serial()
+serialInst.baudrate = 115200
+serialInst.port = "/dev/tty.usbmodem1203"
+serialInst.open()
+
+def get_SerialPorts():
+    return serial.tools.list_ports.comports()
+
+def is_float(string):
+    if string.count(".") == 1:
+        if string.replace(".", "").isnumeric():
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def ReadData():
+    packet = serialInst.readline()
+    while( "$"  not in str(packet) and len(str(packet))!=135):
+        packet = serialInst.readline()
+
+    word=str(packet).strip("b'$").strip("'\\n")
+    lista_parole = word.strip(";").split(",").copy()
+
+    return lista_parole
+
+def check_sum_calculator(strin):
+    somma=[0,0,0,0,0,0,0]
+
+    for lettera in  (strin):
+        check=bin(ord(lettera))[2:]
+        for i in range(0,len(check)):
+            somma[len(somma)-i-1]=somma[len(somma)-i-1]^int(check[len(check)-i-1])
+
+        tot=0
+    for i in range(0,len(somma)):
+        tot+=somma[i]*(2**(len(somma)-1-i))
+    return hex(tot)
